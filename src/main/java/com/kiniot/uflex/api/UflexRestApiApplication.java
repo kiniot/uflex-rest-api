a777cc09d03fc50681a66a52d0fab4b5ee3fcc8d
Salim Ramirez <salimramirezm@gmail.com>
2026-05-01 12:58:16 -0500

    chore: initial commit

---
+++ src/main/java/com/kiniot/uflex/api/UflexRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.kiniot.uflex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UflexRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UflexRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kiniot/uflex/api/UflexRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiniot.uflex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UflexRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
