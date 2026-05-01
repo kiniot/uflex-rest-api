53b65c387333cc366dc2079ded94b7ec25099035
Salim Ramirez <salimramirezm@gmail.com>
2026-05-01 17:04:56 -0500

    feat(iam): add resource classes for user authentication and role management

---
+++ src/main/java/com/kiniot/uflex/api/iam/interfaces/rest/resources/UserResource.java
@@ -0,0 +1,10 @@
+package com.kiniot.uflex.api.iam.interfaces.rest.resources;
+
+import java.util.List;
+
+public record UserResource(
+        String id,
+        String email,
+        List<String> roles
+) {
+}
