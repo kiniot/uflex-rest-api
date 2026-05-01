67dccda204bc0782596e6d30aba3f732370ae1bd
Salim Ramirez <salimramirezm@gmail.com>
2026-05-01 13:54:03 -0500

    feat(app): enable JPA auditing for entity tracking

--- src/main/java/com/kiniot/uflex/api/UflexRestApiApplication.java
+++ src/main/java/com/kiniot/uflex/api/UflexRestApiApplication.java
@@ -2,8 +2,10 @@ package com.kiniot.uflex.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UflexRestApiApplication {
 
     public static void main(String[] args) {
