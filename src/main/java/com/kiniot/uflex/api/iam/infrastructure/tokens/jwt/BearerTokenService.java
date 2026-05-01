b327834510c80af8a1ffa80ea330f9fc0e8f1268
Salim Ramirez <salimramirezm@gmail.com>
2026-05-01 16:24:18 -0500

    feat(iam): implement BCryptHashingService and TokenService for password hashing and JWT token management

---
+++ src/main/java/com/kiniot/uflex/api/iam/infrastructure/tokens/jwt/BearerTokenService.java
@@ -0,0 +1,10 @@
+package com.kiniot.uflex.api.iam.infrastructure.tokens.jwt;
+
+import com.kiniot.uflex.api.iam.application.internal.outboundservices.tokens.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+
+public interface BearerTokenService extends TokenService {
+    String getBearerTokenFrom(HttpServletRequest token);
+    String generateToken(Authentication authentication);
+}
