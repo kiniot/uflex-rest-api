0d87ae31f3cf50fa8bd1bb6ac40cde931e41a6a9
Salim Ramirez <salimramirezm@gmail.com>
2026-05-01 17:07:18 -0500

    feat(shared): add JWT security scheme and requirements to OpenAPI configuration

--- src/main/java/com/kiniot/uflex/api/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/kiniot/uflex/api/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,7 +1,10 @@
 package com.kiniot.uflex.api.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +23,18 @@ public class OpenApiConfiguration {
 
     @Bean
     public OpenAPI iamServiceOpenApi() {
+        // Define the security scheme for JWT
+        var jwtSecurityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // Define the security requirement
+        var securityRequirement =  new SecurityRequirement()
+                .addList("Bearer Authentication");
+
         // Configure API information
         var info = new Info()
                 .title(applicationTitle)
@@ -28,6 +43,9 @@ public class OpenApiConfiguration {
 
         return new OpenAPI()
                 .openapi("3.1.0")
-                .info(info);
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", jwtSecurityScheme));
     }
 }
