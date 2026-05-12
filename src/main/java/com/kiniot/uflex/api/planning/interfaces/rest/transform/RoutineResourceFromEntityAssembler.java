afe8db0ae2abaacb95fcb1c1b5b8aa09b77d14b7
Salim Ramirez <salimramirezm@gmail.com>
2026-05-11 23:50:52 -0500

    fix(planning): enhance resource assemblers for Routine and TreatmentPlan with additional fields

--- src/main/java/com/kiniot/uflex/api/planning/interfaces/rest/transform/RoutineResourceFromEntityAssembler.java
+++ src/main/java/com/kiniot/uflex/api/planning/interfaces/rest/transform/RoutineResourceFromEntityAssembler.java
@@ -1,20 +1,28 @@
 package com.kiniot.uflex.api.planning.interfaces.rest.transform;
 
 import com.kiniot.uflex.api.planning.domain.model.entities.Routine;
+import com.kiniot.uflex.api.planning.interfaces.rest.resources.ExerciseSeriesResource;
 import com.kiniot.uflex.api.planning.interfaces.rest.resources.RoutineResource;
+
+import java.util.List;
 
 public class RoutineResourceFromEntityAssembler {
     public static RoutineResource toResourceFromEntity(Routine entity) {
         var routineId = entity.getId() != null ? entity.getId().id().toString() : null;
-        var exerciseSeries = entity.getExerciseSeries() != null
+        var routineName = entity.getName() != null ? entity.getName().name() : null;
+        var routineOrder = entity.getOrder() != null ? entity.getOrder().value() : null;
+        var schedule = entity.getSchedule() != null
+                ? RoutineScheduleResourceFromValueObjectAssembler.toResourceFromValueObject(entity.getSchedule())
+                : null;
+        List<ExerciseSeriesResource> exerciseSeries = entity.getExerciseSeries() != null
                 ? entity.getExerciseSeries().stream().map(ExerciseSeriesResourceFromValueObjectAssembler::toResourceFromValueObject).toList()
-                : java.util.List.of();
+                : List.of();
 
         return new RoutineResource(
                 routineId,
-                entity.getName().name(),
-                entity.getOrder().value(),
-                RoutineScheduleResourceFromValueObjectAssembler.toResourceFromValueObject(entity.getSchedule()),
+                routineName,
+                routineOrder,
+                schedule,
                 exerciseSeries);
     }
 }
