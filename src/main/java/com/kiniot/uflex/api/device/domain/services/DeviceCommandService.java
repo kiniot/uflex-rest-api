081b5393ef91a23de24a0492f3421c451feb6041
Daniel <u20221g120@upc.edu.pe>
2026-06-04 22:06:45 -0500

    feat(device): add interfaces and implement for query services for device management

---
+++ src/main/java/com/kiniot/uflex/api/device/domain/services/DeviceCommandService.java
@@ -0,0 +1,15 @@
+package com.kiniot.uflex.api.device.domain.services;
+
+import com.kiniot.uflex.api.device.domain.model.aggregates.Device;
+import com.kiniot.uflex.api.device.domain.model.commands.*;
+import com.kiniot.uflex.api.shared.domain.model.valueobjects.ClinicId;
+
+public interface DeviceCommandService {
+    Device handle(RegisterDeviceCommand command, ClinicId clinicId);
+    void handle(AssignDeviceToPatientCommand command);
+    void handle(ReturnDeviceCommand command);
+    void handle(UpdateDeviceTelemetryCommand command);
+    void handle(RegisterSuccessfulCalibrationCommand command);
+    void handle(MarkCalibrationAsInvalidCommand command);
+    void handle(UpdateDeviceStatusCommand command);
+}
