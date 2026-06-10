716c75cbab8dd1659afd3f6fa6d6b9f3784bbdf0
Kyrubi <u20221c486@upc.edu.pe>
2026-06-10 09:35:06 -0500

    refactor(planning): update PlanningContextFacade

--- src/main/java/com/kiniot/uflex/api/planning/interfaces/acl/PlanningContextFacade.java
+++ src/main/java/com/kiniot/uflex/api/planning/interfaces/acl/PlanningContextFacade.java
@@ -1,5 +1,13 @@
 package com.kiniot.uflex.api.planning.interfaces.acl;
 
+import com.kiniot.uflex.api.planning.interfaces.acl.dto.RoutineDetailsDto;
+
 public interface PlanningContextFacade {
     boolean existsTreatmentPlanByPatientId(String patientId);
+
+    RoutineDetailsDto getRoutineDetails(String routineId);
+
+    boolean isRoutineInPatientTreatmentPlan(String routineId, String treatmentPlanId, String patientId);
+
+    void onTherapySessionCompleted(String sessionId, String patientId, String finalizedAt);
 }
