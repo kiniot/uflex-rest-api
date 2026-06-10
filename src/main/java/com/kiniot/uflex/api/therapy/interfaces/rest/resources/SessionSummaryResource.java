2582e4443d0053516cd44fbf3edcd90234f2604f
Kyrubi <u20221c486@upc.edu.pe>
2026-06-10 09:35:20 -0500

    feat(therapy): update SessionSummaryResource

---
+++ src/main/java/com/kiniot/uflex/api/therapy/interfaces/rest/resources/SessionSummaryResource.java
@@ -0,0 +1,18 @@
+package com.kiniot.uflex.api.therapy.interfaces.rest.resources;
+
+import lombok.Builder;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Builder
+public record SessionSummaryResource(
+        UUID sessionId,
+        UUID patientId,
+        Integer totalSeries,
+        Integer completedSeries,
+        Integer painLevel,
+        Integer anomaliesDetected,
+        Instant startedAt,
+        Instant finalizedAt
+) {}
