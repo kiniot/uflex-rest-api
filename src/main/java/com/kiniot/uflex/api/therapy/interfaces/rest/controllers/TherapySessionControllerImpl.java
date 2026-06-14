691c0f78545255d79822f84f0343cce5d5089430
Kyrubi <u20221c486@upc.edu.pe>
2026-06-14 04:27:04 -0500

    feat(therapy): add method to retrieve daily schedule for a patient

--- src/main/java/com/kiniot/uflex/api/therapy/interfaces/rest/controllers/TherapySessionControllerImpl.java
+++ src/main/java/com/kiniot/uflex/api/therapy/interfaces/rest/controllers/TherapySessionControllerImpl.java
@@ -1,14 +1,17 @@
 package com.kiniot.uflex.api.therapy.interfaces.rest.controllers;
 
 import com.kiniot.uflex.api.therapy.domain.model.queries.GetActiveTherapySessionByPatientIdQuery;
+import com.kiniot.uflex.api.therapy.domain.model.queries.GetDailyScheduleQuery;
 import com.kiniot.uflex.api.therapy.domain.model.queries.GetSessionSummaryQuery;
 import com.kiniot.uflex.api.therapy.domain.services.TherapySessionCommandService;
 import com.kiniot.uflex.api.therapy.domain.services.TherapySessionQueryService;
 import com.kiniot.uflex.api.therapy.interfaces.rest.resources.CancelTherapySessionResource;
 import com.kiniot.uflex.api.therapy.interfaces.rest.resources.ConfirmHardwareReadinessResource;
+import com.kiniot.uflex.api.therapy.interfaces.rest.resources.DailyScheduleResource;
 import com.kiniot.uflex.api.therapy.interfaces.rest.resources.InitiateTherapyPreparationResource;
 import com.kiniot.uflex.api.therapy.interfaces.rest.resources.SessionSummaryResource;
 import com.kiniot.uflex.api.therapy.interfaces.rest.resources.TherapySessionResource;
+import com.kiniot.uflex.api.therapy.interfaces.rest.transform.DailyScheduleResourceFromDtoAssembler;
 import com.kiniot.uflex.api.therapy.interfaces.rest.transform.SessionSummaryResourceFromEntityAssembler;
 import com.kiniot.uflex.api.therapy.interfaces.rest.transform.TherapySessionResourceFromEntityAssembler;
 import com.kiniot.uflex.api.therapy.interfaces.rest.transform.CancelTherapySessionCommandFromResourceAssembler;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @RestController
@@ -81,5 +85,12 @@ public class TherapySessionControllerImpl implements TherapySessionController {
     public ResponseEntity<SessionSummaryResource> getSessionSummary(UUID id) {
         var session = therapySessionQueryService.handle(new GetSessionSummaryQuery(id));
         return ResponseEntity.ok(SessionSummaryResourceFromEntityAssembler.toResponseFromEntity(session));
+    }
+
+    @Override
+    public ResponseEntity<DailyScheduleResource> getDailySchedule(UUID patientId, LocalDate date) {
+        LocalDate target = date != null ? date : LocalDate.now();
+        var routine = therapySessionQueryService.handle(new GetDailyScheduleQuery(patientId, target));
+        return ResponseEntity.ok(DailyScheduleResourceFromDtoAssembler.toResourceFromDto(patientId, target, routine));
     }
 }
