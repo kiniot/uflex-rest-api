9aa2620b83a5fa279e7556f82a6855d721860653
Kyrubi <u20221c486@upc.edu.pe>
2026-06-16 12:34:05 -0500

    feat(therapy): update RepetitionRecorded

--- src/main/java/com/kiniot/uflex/api/therapy/domain/model/events/RepetitionRecorded.java
+++ src/main/java/com/kiniot/uflex/api/therapy/domain/model/events/RepetitionRecorded.java
@@ -1,9 +1,11 @@
 package com.kiniot.uflex.api.therapy.domain.model.events;
 
 import lombok.Getter;
+import org.springframework.cglib.core.Local;
 import org.springframework.context.ApplicationEvent;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Getter
 public class RepetitionRecorded extends ApplicationEvent {
@@ -11,8 +13,8 @@ public class RepetitionRecorded extends ApplicationEvent {
     private final String sessionId;
     private final String serieId;
     private final Double achievedAngle;
-    private final Instant recordedAt;
-    private final Instant occurredAt;
+    private final LocalDateTime recordedAt;
+    private final LocalDateTime occurredAt;
 
     private RepetitionRecorded(Builder builder) {
         super(builder.source);
@@ -20,7 +22,7 @@ public class RepetitionRecorded extends ApplicationEvent {
         this.serieId = builder.serieId;
         this.achievedAngle = builder.achievedAngle;
         this.recordedAt = builder.recordedAt;
-        this.occurredAt = Instant.now();
+        this.occurredAt = LocalDateTime.now();
     }
 
     public static Builder builder() { return new Builder(); }
@@ -30,13 +32,13 @@ public class RepetitionRecorded extends ApplicationEvent {
         private String sessionId;
         private String serieId;
         private Double achievedAngle;
-        private Instant recordedAt;
+        private LocalDateTime recordedAt;
 
         public Builder source(Object source)         { this.source = source; return this; }
         public Builder sessionId(String v)           { this.sessionId = v; return this; }
         public Builder serieId(String v)             { this.serieId = v; return this; }
         public Builder achievedAngle(Double v)       { this.achievedAngle = v; return this; }
-        public Builder recordedAt(Instant v)         { this.recordedAt = v; return this; }
+        public Builder recordedAt(LocalDateTime v)         { this.recordedAt = v; return this; }
 
         public RepetitionRecorded build()            { return new RepetitionRecorded(this); }
     }
